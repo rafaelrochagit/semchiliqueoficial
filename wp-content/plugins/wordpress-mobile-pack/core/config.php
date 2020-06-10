d6de9900536d2a6e488aa2a17cbd2fc97ec48b45
Kleberson Rocha <klebersonx.rocha@gmail.com>
2020-06-10 19:05:28 -0300

    installed plugin `WordPress Mobile Pack` version 3.4.1

---
+++ wp-content/plugins/wordpress-mobile-pack/core/config.php
@@ -0,0 +1,28 @@
+<?php
+
+define("WMP_VERSION", '3.4.1');
+define('WMP_PLUGIN_NAME', 'WP Mobile Pack');
+define('WMP_DOMAIN', 'wordpress-mobile-pack');
+
+define('WMP_PLUGIN_PATH', WP_PLUGIN_DIR . '/'.WMP_DOMAIN.'/');
+
+define('WMP_FEEDBACK_EMAIL','support@wpmobilepack.com');
+
+define('WMP_NEWS_UPDATES','https://rcksld-wpmp.s3.amazonaws.com/dashboard/news.json');
+define('WMP_NEWS_UPDATES_HTTPS','https://rcksld-wpmp.s3.amazonaws.com/dashboard/news.json');
+
+define('WMP_WHATSNEW_UPDATES','https://rcksld-wpmp.s3.amazonaws.com/dashboard/quick_start/content.json');
+define('WMP_WHATSNEW_UPDATES_HTTPS','https://rcksld-wpmp.s3.amazonaws.com/dashboard/quick_start/content_https.json');
+
+define('WMP_MORE_UPDATES','https://rcksld-wpmp.s3.amazonaws.com/dashboard/more/more5.json');
+define('WMP_MORE_UPDATES_HTTPS','https://rcksld-wpmp.s3.amazonaws.com/dashboard/more/more5.json');
+define('WMP_MORE_UPDATES_VERSION', 6);
+
+define('WMP_APPTICLES_TRACKING_SSL','https://api.wpmobilepack.com/content1/wptracking');
+
+define("WMP_APPTICLES_PRO_LINK", "https://wpmobilepack.com");
+
+// define the string used for generating comments tokens (can be overwritten for increasing security)
+define('WMP_CODE_KEY','asdc%/dfr_A!8792*');
+
+?>
