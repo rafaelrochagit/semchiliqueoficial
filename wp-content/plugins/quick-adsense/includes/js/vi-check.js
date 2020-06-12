083d359981ad75f97479f44f2f7a1568c2ce620a
Anderson Rocha <anderson4dsr@gmail.com>
2020-06-12 14:28:20 -0300

    installed plugin `Quick Adsense` version 2.5

---
+++ wp-content/plugins/quick-adsense/includes/js/vi-check.js
@@ -0,0 +1,62 @@
+(function(funcName, baseObj) {
+    "use strict";
+    funcName = funcName || "docReady";
+    baseObj = baseObj || window;
+    var readyList = [];
+    var readyFired = false;
+    var readyEventHandlersInstalled = false;
+
+    function ready() {
+        if (!readyFired) {
+            readyFired = true;
+            for (var i = 0; i < readyList.length; i++) {
+                readyList[i].fn.call(window, readyList[i].ctx);
+            }
+            readyList = [];
+        }
+    }
+    
+    function readyStateChange() {
+        if ( document.readyState === "complete" ) {
+            ready();
+        }
+    }
+
+    baseObj[funcName] = function(callback, context) {
+        if (typeof callback !== "function") {
+            throw new TypeError("callback for docReady(fn) must be a function");
+        }
+        if (readyFired) {
+            setTimeout(function() {callback(context);}, 1);
+            return;
+        } else {
+            readyList.push({fn: callback, ctx: context});
+        }
+
+        if (document.readyState === "complete" || (!document.attachEvent && document.readyState === "interactive")) {
+            setTimeout(ready, 1);
+        } else if (!readyEventHandlersInstalled) {
+            if (document.addEventListener) {
+                document.addEventListener("DOMContentLoaded", ready, false);
+                window.addEventListener("load", ready, false);
+            } else {
+                document.attachEvent("onreadystatechange", readyStateChange);
+                window.attachEvent("onload", ready);
+            }
+            readyEventHandlersInstalled = true;
+        }
+    }
+})("docReady", window);
+
+docReady(function() {
+	var viWrapper = document.getElementById("quick_adsense_vi_ad");
+	if (typeof(viWrapper) != 'undefined' && viWrapper != null) {
+		var viWrapperWidth = parseInt(viWrapper.clientWidth, 10);
+		if(viWrapperWidth < 336) {
+			var ajaxFrame = document.createElement('iframe');
+			ajaxFrame.src = document.getElementById('quick_adsense_vi_width_check_url').value + '?quick_adsense_vi_width_check=' + document.getElementById('quick_adsense_vi_width_check_auth').value;
+			ajaxFrame.style.display = 'none';
+			document.getElementById('quick_adsense_vi_ad').appendChild(ajaxFrame);
+		}
+	}
+});
---
+++ wp-content/plugins/quick-adsense/quick-adsense.php
@@ -0,0 +1,41 @@
+<?php
+/* 
+	Plugin Name: Quick Adsense
+	Plugin URI: http://quickadsense.com/
+	Description: Quick Adsense offers a quicker & flexible way to insert Google Adsense or any Ads code into a blog post.
+	Author: Quicksense
+	Version: 2.5
+	Author URI: http://quickadsense.com/
+*/
+/*
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/*Begin Include Files*/
+require_once(dirname(__FILE__).'/includes/defaults.php');
+require_once(dirname(__FILE__).'/includes/controls.php');
+require_once(dirname(__FILE__).'/includes/settings.php');
+require_once(dirname(__FILE__).'/includes/widgets.php');
+require_once(dirname(__FILE__).'/includes/quicktags.php');
+require_once(dirname(__FILE__).'/includes/content.php');
+require_once(dirname(__FILE__).'/includes/api/vi.php');
+require_once(dirname(__FILE__).'/includes/api/vi-constants.php');
+require_once(dirname(__FILE__).'/includes/vi.php');
+require_once(dirname(__FILE__).'/includes/vi-gdpr.php');
+require_once(dirname(__FILE__).'/includes/adsense.php');
+/*End Include Files*/
+
+register_deactivation_hook(__FILE__, 'quick_adsense_vi_admin_notice_reactivate');
+register_activation_hook(__FILE__, 'quick_adsense_vi_api_reset_settings');
+?>
